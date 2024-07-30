06520715661bf983bee9a6dfeeba38dc9bbbd744
Hugo JL <hugojl@gmail.com>
2024-07-30 18:44:09 +0300

    Added MadCalcService to app.Component. Request works OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import { NavigationBarComponent } from "./common/components/core/navigation-bar/navigation-bar.component";
-import { SearchWidgetComponent } from "./common/components/shared/search-widget/search-widget.component";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NavigationBarComponent } from './common/components/core/navigation-bar/navigation-bar.component';
+import { SearchWidgetComponent } from './common/components/shared/search-widget/search-widget.component';
+import { MadCalcService } from '../app/common/services/mad-calc.service';
+import { MadCalcApiResponse } from '../app/common/models';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, MatButtonModule, MatToolbarModule, FlexLayoutModule, NavigationBarComponent, SearchWidgetComponent],
+  imports: [
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    NavigationBarComponent,
+    SearchWidgetComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'madcalc-V3-app';
+  madCalcData: MadCalcApiResponse | undefined;
+
+  constructor(private madCalcService: MadCalcService) {}
+
+  ngOnInit(): void {
+    this.madCalcService.getData().subscribe(
+      (response: MadCalcApiResponse) => {
+        this.madCalcData = response;
+        console.log(this.madCalcData);
+      },
+      (error: any) => {
+        console.error('Error fetching data', error);
+      }
+    );
+  }
 }
